bceb0b7976a571005f503f5eb2c47e2ed5a79b15
Narotam-Mishra <narotam21jan@gmail.com>
2025-02-19 16:03:41 +0530

    shadcn_added_changes

--- shadcn-basics/app/page.tsx
+++ shadcn-basics/app/page.tsx
@@ -1,21 +1,28 @@
 "use client"
 
+// import MyAvatar from "./shadcn-components/MyAvatar";
+
+// import { useSearchParams } from "next/navigation";
 // import MyAlertDialog from "./shadcn-components/MyAlertDialog";
 
 // import MyAccordion from "./shadcn-components/MyAccordion";
-import MyAspectRatio from "./shadcn-components/MyAspectRatio";
+// import MyAspectRatio from "./shadcn-components/MyAspectRatio";
 // import MyAlert from "./shadcn-components/MyAlert";
 
 export default function Home() {
-  
+  // const params = useSearchParams()
+  // const balance = params.get("balance");
+
   return (
     <div className="w-[200px]">
       {/* <MyAccordion /> */}
+      {/* <MyAlert /> */}
       {/* {
         balance ? <MyAlert /> : ""
       } */}
       {/* <MyAlertDialog title="Open" content="Are you sure?" component={<MyAccordion/>} /> */}
-      <MyAspectRatio />
+      {/* <MyAspectRatio /> */}
+      {/* <MyAvatar /> */}
     </div>
   );
 }
--- shadcn-basics/app/shadcn-components/MyAspectRatio.tsx
+++ shadcn-basics/app/shadcn-components/MyAspectRatio.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
-// import Image from "next/image"
+import Image from "next/image"
 
-// import { AspectRatio } from "@/components/ui/aspect-ratio"
 import MyAvatar from './MyAvatar';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
 
 const MyAspectRatio = () => {
   return (
     <div className='w-[350px]'>
-      {/* <AspectRatio ratio={1 / 1}>
+      <AspectRatio ratio={1 / 1}>
         <Image
           src="https://images.unsplash.com/photo-1733690577845-4f4641a456b3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
           alt="Image"
           className="rounded-md object-cover"
           fill
         />
-      </AspectRatio> */}
+      </AspectRatio>
       <MyAvatar />
     </div>
   );
